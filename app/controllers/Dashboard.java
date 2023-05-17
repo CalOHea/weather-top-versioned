303a2f86d164e06fcaacc079645115a137cc15fe
CallumFarrell <20104331@mail.wit.ie>
2023-05-17 01:09:01 +0100

    Added Member Functionality

--- app/controllers/Dashboard.java
+++ app/controllers/Dashboard.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import models.Member;
 import play.Logger;
 import play.mvc.Controller;
 
@@ -14,16 +15,19 @@ public class Dashboard extends Controller
 
   public static void index() {
     Logger.info("Rendering Dashboard");
+    Member member = Accounts.getLoggedInMember();
 
-    List<Station> stations = Station.findAll();
+    List<Station> stations = member.stations;
 
     render ("dashboard.html", stations);
   }
 
   public static void addStation (String name) {
+    Logger.info("Adding a new station");
+    Member member= Accounts.getLoggedInMember();
     Station station = new Station (name);
-    Logger.info("Adding a new station called " + name);
-    station.save();
+    member.stations.add(station);
+    member.save();
     redirect ("/dashboard");
   }
 
