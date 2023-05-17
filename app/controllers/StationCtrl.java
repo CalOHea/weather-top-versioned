a1a3c6470aab29769592e8af656001520cf12c2c
CallumFarrell <20104331@mail.wit.ie>
2023-05-17 21:52:36 +0100

    Added form validation for the Code in addReading

--- app/controllers/StationCtrl.java
+++ app/controllers/StationCtrl.java
@@ -12,16 +12,20 @@ public class StationCtrl extends Controller {
 
     public static void index(Long id) {
         Station station = Station.findById(id);
-        Logger.info ("Station id = " + id);
+        Logger.info("Station id = " + id);
         render("station.html", station);
     }
 
     public static void addReading(Long id, int code, double temperature, double windSpeed, double windDirection, int pressure) {
-        Reading reading = new Reading(code, temperature, windSpeed, windDirection, pressure);
-        Station station = Station.findById(id);
-        station.readings.add(reading);
-        station.save();
-        redirect ("/stations/" + id);
+        if (code == 0) {
+            flash("error", "Please select a Code from the dropdown");
+            redirect("/stations/" + id);
+        } else {
+            Reading reading = new Reading(code, temperature, windSpeed, windDirection, pressure);
+            Station station = Station.findById(id);
+            station.readings.add(reading);
+            station.save();
+            redirect("/stations/" + id);
+        }
     }
-
 }
